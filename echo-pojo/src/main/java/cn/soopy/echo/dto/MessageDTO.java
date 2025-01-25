015f80d3515203de42a72769ea20153116445e18
L1nSn0w <l1nsn0w@qq.com>
2025-01-25 20:33:00 +0800

    chore: simplify some unnecessary file😊

--- echo-pojo/src/main/java/cn/soopy/echo/dto/MessageDTO.java
+++
@@ -1,7 +0,0 @@
-package cn.soopy.echo.dto;
-
-import lombok.Data;
-
-@Data
-public class MessageDTO {
-}
--- echo-pojo/src/main/java/cn/soopy/echo/dto/UserDTO.java
+++
@@ -1,8 +0,0 @@
-package cn.soopy.echo.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDTO {
-
-}
